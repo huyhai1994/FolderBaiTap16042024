502893fc5f6b54af00a6984a19fd85839c3bd09c
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-04-17 06:28:08 +0700

    lenovo_huyhai1994

--- src/aarraylist/ArrayListSelf.java
+++ src/aarraylist/ArrayListSelf.java
@@ -10,21 +10,47 @@ public class ArrayListSelf {
     }
 
     public void add(Object object) {
+        boolean isTheListIsFull = (size >= container.length);
+        if(isTheListIsFull) {
+            Object [] newArrayTwiceOfCurrentArrayCapacity = new Object[container.length * 2];
+            System.arraycopy(container, 0,
+                    newArrayTwiceOfCurrentArrayCapacity, 0, container.length);
+            container = newArrayTwiceOfCurrentArrayCapacity;
+        }
         this.container[size] = object;
         size++;
     }
 
     public Object get(int index) {
-
         return this.container[index];
     }
 
     public void update(int index, Object object) {
+        this.container[index] = object;
     }
 
     public void insert(int index, Object object) {
+        for (int i = size; i > index; i--) {
+            this.container[i] = this.container[i - 1];
+        }
+        this.container[index] = object;
+        size++;
     }
-
+    public void remove(int index) {
+        for (int i = index; i < size - 1; i++) {
+            this.container[i] = this.container[i + 1];
+        }
+        this.container[size - 1] = null;
+        size--;
+    }
+    public void removeFirst(){
+        this.container[0] = null;
+        size--;
+    }
+    public void removeLast(){
+        this.container[size - 1] = null;
+        size--;
+    }
     public void readList() {
         for (Object object : container) {
             boolean isObjectNotNull = object != null;
--- src/aarraylist/ArrayListTest.java
+++ src/aarraylist/ArrayListTest.java
@@ -1,12 +1,25 @@
 package aarraylist;
 
+import alinkedlist.Student;
+
 public class ArrayListTest {
     public static void main(String[] args) {
         ArrayListSelf arrayListSelf = new ArrayListSelf();
-        arrayListSelf.add("1");
-        arrayListSelf.add("2");
-        arrayListSelf.add("3");
-        arrayListSelf.add("4");
+        Student student1 = new Student(1, "Thuy", "nguyenthuy123@gmail.com", 8.0);
+        Student student2 = new Student(2, "Mai", "nguyenmaix123@gmail.com", 5.0);
+        Student student3 = new Student(3, "Thanh", "nguyenThanh123@gmail.com", 9.0);
+        Student student4 = new Student(4, "Hai", "nguyenhai926@gmail.com", 7.0);
+        Student student5 = new Student(5, "Huong", "nguyenHuong123@gmail.com", 10.0);
+        Student student6 = new Student(6, "Duy", "nguyenduy321@gmail.com", 6.0);
+        arrayListSelf.add(student1);
+        arrayListSelf.add(student2);
+        arrayListSelf.add(student3);
+        arrayListSelf.add(student4);
+        arrayListSelf.add(student5);
+        arrayListSelf.add(student6);
+
+        arrayListSelf.readList();
+        arrayListSelf.remove(3);
         arrayListSelf.readList();
     }
 }
